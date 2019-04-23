d7492bd7f536da4f2d8d63e042494b7b91cc4f09
mario Lafuente <mlafuente@gmail.com>
2019-04-22 23:06:06 -0600

    new ver from compuCasa lec 16

--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,11 @@ import  Header  from  './Header';
 import  Order   from  './Order';
 import  Inventory   from  './Inventory';
 import sampleFishes from '../sample-fishes'; //minuscula cuando no es class
-import FishItem from './FishItem';
+import Fish from './Fish';
+
 
 class App extends React.Component {
-  
+
   //1 Creo un objeto state
   state = {
     fishes:{},
@@ -33,23 +34,39 @@ class App extends React.Component {
     { fishes: sampleFishes}
     );
   }
+
+  addToOrder = (key) => {
+    //1. take a copy of state
+    const order = {...this.state.order}
+    // 2. add order or update the number // si order1 existe sumele otra If sino es 1
+    order[key] = order[key] + 1 || 1;
+
+    //3. call set state to update state object
+  this.setState(
+    {order : order}
+  );
+
+  }
+
+
+
   render() {
     return (
 
     <div className="catch-of-the-day">
 
       <div className="menu">
-        <Header tagline='mario is cool' age={43} cool={true} />
+        <Header tagline='Fresh Market' age={43} cool={true} />
         <ul className="fishes">
-          {Object.keys(this.state.fishes).map(key => <FishItem key={key} />)} 
+          {Object.keys(this.state.fishes).map(key => <Fish key={key} index={key} detalle={this.state.fishes[key]} addToOrder={this.addToOrder}  />)}
           {/*es como n loop de objeto */}
         </ul>
       </div>
 
       <Order />
-      <Inventory  
-      addFish={this.addFish} 
-      loadExamples={this.loadExamples} 
+      <Inventory
+      addFish={this.addFish}
+      loadExamples={this.loadExamples}
       />
 
     </div>
