f439d577c3a1c2d940c4c7c4207abaaba34390b9
mario-lafuente <info@mariolafuente.com>
2019-04-27 13:11:00 -0600

    les 22

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { formatPrice } from '../helpers';
+import {TransitionGroup, CSSTransition } from 'react-transition-group';
+
 class Order extends React.Component {
   
   removeFish = (key) => {
@@ -16,17 +18,23 @@ class Order extends React.Component {
     const isAvailable = fish.status === 'available';
     
     
-
+    //
     if(!isAvailable){
       return <li key={key} > Sorry {fish.name} is no longer available</li>;
     }else{
           return(
+
+            <CSSTransition classNames='order' key={key}
+            timeout={{ enter:250, exit:250 }}
+            >
+
           <li key={key} >
             {count} lbs {fish.name}
             {formatPrice(count*fish.price)} {/* formatPrice viene de helper formatea el preio  */}
             {/* remove from order en una linea para pasarle el key */}
             <button onClick={() => this.props.deleteFishFromOrder(key)}>&times;</button>
           </li>
+          </CSSTransition>
           )
     }
   }
@@ -52,10 +60,12 @@ class Order extends React.Component {
     return (
      <div className="order-wrap">
       <h2>Order</h2>
-      <ul>
+
+      <TransitionGroup component='ul'  className='order'>
       {/* { orderIds.map(key => <li>{key}</li>) } asi o en funcion */}
       { orderIds.map(this.renderOrder) }
-      </ul>
+      </TransitionGroup>
+
       <div className="total">
       Total:
       <strong> { formatPrice(total) }</strong>
